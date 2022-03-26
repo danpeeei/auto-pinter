7bcde2bb7271f11ec5f1376f6ddef5ab462392c6
danpeeei <52124762+danpeeei@users.noreply.github.com>
2022-03-26 12:15:53 +0900

    check working in Raspberry Pi 3 (#1)
    * add logger
    
    * refactor: use class
    
    * fix: ignore exception when importing chromedriver-binary
    
    * fix: bug in headless logic
    
    * refactor: add info log
    
    * refactor: rename class methods
    
    * fix: bug that url is not opened
    
    * fix: bug in parse_board_name
    
    * refactor: simplify loop
    
    * refactor: remove unnecessary log

--- auto-pinter/main.py
+++ auto-pinter/main.py
@@ -1,87 +1,133 @@
+import logging
 from argparse import ArgumentParser
+from urllib.parse import unquote
 
-import chromedriver_binary  # noqa: F401
+try:
+    import chromedriver_binary  # noqa: F401
+except Exception:
+    pass
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
-BASE_URL = "https://www.pinterest.jp"
-driver = webdriver.Chrome()
-driver.get(BASE_URL)
-driver.implicitly_wait(10)
+BASE_URL = "https://www.pinterest.jp/"
+LOG = logging.getLogger(__name__)
 
-board_tab = None
+logging.basicConfig(level=logging.INFO)
 
 
-def fill_input_by_test_id(text: str, test_id: str):
-    input = driver.find_element(
-        By.XPATH, f'//*[@data-test-id="{test_id}"]'
-    ).find_element(By.TAG_NAME, "input")
-    input.send_keys(text)
+def parse_board_name(url: str):
+    decoded = unquote(url).rstrip("/")
+    return decoded.split("/")[-1]
 
 
-def click_by_test_id(test_id: str):
-    button = driver.find_element(By.XPATH, f"//*[@data-test-id='{test_id}']")
-    button.click()
+class AutoPinter:
+    def __init__(self, debug: bool = True) -> None:
+        options = Options()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-gpu")
+        if not debug:
+            options.add_argument("--headless")
+        self._driver = webdriver.Chrome(options=options)
+        self._driver.implicitly_wait(10)
 
+    def _fill_input_by_test_id(self, text: str, test_id: str):
+        input = self._driver.find_element(
+            By.XPATH, f'//*[@data-test-id="{test_id}"]'
+        ).find_element(By.TAG_NAME, "input")
+        input.send_keys(text)
 
-def login(username: str, password: str):
-    """ログイン"""
-    click_by_test_id("simple-login-button")
+    def _click_by_test_id(self, test_id: str):
+        button = self._driver.find_element(By.XPATH, f"//*[@data-test-id='{test_id}']")
+        button.click()
 
-    fill_input_by_test_id(username, "emailInputField")
-    fill_input_by_test_id(password, "passwordInputField")
+    def start_process(self, user: str, password: str):
+        """各ボードに対しておすすめのピンを追加する"""
+        self._open()
+        self._login(user, password)
+        self._show_saved_contents()
 
-    click_by_test_id("registerFormSubmitButton")
+        boards = self._get_board_list()
+        n = len(boards)
+        LOG.info(f"found {n} boards")
+        for i, board in enumerate(boards, start=1):
+            self._add_pin_to_board(board)
+            LOG.info(f"({i}/{n}) board '{parse_board_name(board)}': ok")
+        LOG.info(f"successfully updated {n} boards")
 
-    # move to profile
-    click_by_test_id("header-profile")
+    def close(self):
+        self._driver.close()
 
+    def _open(self):
+        self._driver.get(BASE_URL)
+        LOG.info(f"successfully opened {BASE_URL}")
 
-def get_board_list():
-    boards = driver.find_elements(By.XPATH, "//*[@data-test-id='pwt-grid-item']")
-    result = []
-    # 先頭は"すべてのピン"のため無視
-    for board in boards[1:]:
-        a: WebElement = board.find_element(By.TAG_NAME, "a")
-        href = a.get_attribute("href")
-        result.append(href)
-    return result
+    def _login(self, username: str, password: str):
+        self._click_by_test_id("simple-login-button")
 
+        self._fill_input_by_test_id(username, "emailInputField")
+        self._fill_input_by_test_id(password, "passwordInputField")
 
-def add_pin_to_board(board_url: str):
-    # "おすすめのアイデア"を新規タブで開く
-    try:
-        url = board_url + "_tools/more-ideas/?ideas_referrer=2"
-        driver.execute_script("window.open(arguments[0], 'newtab')", url)
-        driver.switch_to.window(driver.window_handles[1])
+        self._click_by_test_id("registerFormSubmitButton")
 
-        # 先頭のピンをボードに追加
-        pin = driver.find_element(By.XPATH, "//*[@data-test-id='pin']")
-        save = pin.find_element(By.XPATH, "//div[@aria-label='保存']")
-        save.click()
-    except Exception:
-        pass
-    finally:
-        # タブを閉じる
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+        LOG.info("logged in")
+
+    def _show_saved_contents(self):
+        # move to profile
+        self._click_by_test_id("header-profile")
+
+        # show saved contents
+        self._driver.find_element(By.ID, "_saved-profile-tab").click()
+
+        LOG.info(f"showed saved contents. current url: {self._driver.current_url}")
+
+    def _get_board_list(self):
+        boards = self._driver.find_elements(
+            By.XPATH, "//*[@data-test-id='pwt-grid-item']"
+        )
+        result = []
+        # 先頭は"すべてのピン"のため無視
+        for board in boards[1:]:
+            a: WebElement = board.find_element(By.TAG_NAME, "a")
+            result.append(a.get_attribute("href"))
+        return result
+
+    def _add_pin_to_board(self, board_url: str):
+        # "おすすめのアイデア"を新規タブで開く
+        try:
+            url = board_url + "_tools/more-ideas/?ideas_referrer=2"
+            self._driver.execute_script("window.open(arguments[0], 'newtab')", url)
+            self._driver.switch_to.window(self._driver.window_handles[1])
+
+            # 先頭のピンをボードに追加
+            pin = self._driver.find_element(By.XPATH, "//*[@data-test-id='pin']")
+            save = pin.find_element(By.XPATH, "//div[@aria-label='保存']")
+            save.click()
+        except Exception as e:
+            LOG.error(e)
+        finally:
+            # タブを閉じる
+            self._driver.close()
+            self._driver.switch_to.window(self._driver.window_handles[0])
 
 
 def main():
     parser = ArgumentParser("auto-pinter")
+    parser.add_argument("-d", "--debug", action="store_true")
     parser.add_argument("-u", "--user", required=True)
     parser.add_argument("-p", "--password", required=True)
     args = parser.parse_args()
 
-    login(args.user, args.password)
-    for board in get_board_list():
-        add_pin_to_board(board)
+    pinter = AutoPinter(args.debug)
+    try:
+        pinter.start_process(args.user, args.password)
+    except Exception as e:
+        LOG.error(e)
+    finally:
+        pinter.close()
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    driver.close()
+    main()
